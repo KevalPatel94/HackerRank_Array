d7964228e1f740747d11971a973920e6f33ec639
Keval Patel <kevaltpatel@gmail.com>
2018-01-09 11:26:51 -0600

    no message

---
+++ HackerArray1.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+
+class DataStructure
+{
+       ArrayList arr = new ArrayList();
+    
+      public void displayAarray()
+    {
+        for(int index = arr.size() - 1;index >= 0;index--)
+        {
+            System.out.print(arr.get(index) + " ");
+        }
+    }
+}
+public class HackerArray1 {
+    
+    public static void main(String[] args) {
+        
+        
+        Scanner scan = new Scanner(System.in);
+        
+        
+        DataStructure struct = new DataStructure();
+        int arraySize = scan.nextInt();
+
+        for(int i = 0; i < arraySize; i++)
+        {
+            int element = scan.nextInt();
+            struct.arr.add(i,element);
+        }
+        
+
+        struct.displayAarray();
+    } 
+}
